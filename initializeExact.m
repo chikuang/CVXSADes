f154a09421f8eea274c38457184b99ae8abb57fa
Chi-Kuang <chi-kuang.yeh@uwaterloo.ca>
2023-08-21 14:35:59 -0400

    Add - scripts attached in the email
    The initialization matlab scripts

---
+++ initializeExact.m
@@ -0,0 +1,15 @@
+function We = initializeExact(W,N)
+% initializeExact(W,N): given list of weights W for approximate design, and
+% total number of design points N, return modified weight list We such that
+% for each w[i] in We, N*w gives the exact integer number of points for
+% the ith support point in the exact design
+ 
+We = floor(N*W); % take floor of approx design*N, sum(We) <= N initially
+
+i = 1;
+while(sum(We) < N)  
+    We(i) = We(i) + 1; % lazily increase number of         
+    i = i + 1;         % support points until we have N of them
+end
+
+We = We/N;
