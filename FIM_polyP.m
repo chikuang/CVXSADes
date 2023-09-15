fb2cbad3c2a58379c57e6c6aa87139bda1061326
Chi-Kuang <chi-kuang.yeh@uwaterloo.ca>
2023-09-15 12:17:57 -0400

    Fix the function that calculate the fisher information matrix for poly

---
+++ FIM_polyP.m
@@ -0,0 +1,13 @@
+function FIM_polyP = FIM_1dregP3(x, p)
+  % fim_3dlinreg(x): return Fisher Information Matrix terms given design space x
+  % for a 3D linear regression model
+  Z = power(x(:), 0:p);  
+     
+  q = size(Z,2);
+  
+  FIM_polyP = zeros(q,q,length(x));
+  
+  for i = 1:length(x)
+      FIM_polyP(:,:,i) = Z(i,:)' * Z(i,:);
+  end
+end
