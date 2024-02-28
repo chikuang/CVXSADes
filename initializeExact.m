54dcf14cf97ec993250bd67940bdfd70923952f8
Chi-Kuang Yeh <chi-kuang.yeh@uwaterloo.ca>
2024-02-27 20:40:28 -0500

    Modify - initialization exact design - with modulos
    To prevent possible error by going out of bound

--- initializeExact.m
+++ initializeExact.m
@@ -9,7 +9,7 @@ We = floor(N*W); % take floor of approx design*N, sum(We) <= N initially
 i = 1;
 while(sum(We) < N)  
     We(i) = We(i) + 1; % lazily increase number of         
-    i = i + 1;         % support points until we have N of them
+    i =  mod(i+1, N) + 1;         % support points until we have N of them
 end
 
 We = We/N;
