578e9f65a8e81e6807c4560d194a1c356e5c8b0e
Chi-Kuang Yeh <chi-kuang.yeh@uwaterloo.ca>
2024-05-04 09:51:13 -0600

    Add - plotting scripts

--- paper/plots/plot_SA_app1_N21.R
+++ paper/plots/plot_SA_app1_N21.R
@@ -1,27 +1,25 @@
-dat <- read.csv("./paper/plots/app3_c-opt_n12.csv",
+# dat <- read.csv("./../app2-group/app3_c-opt_n12.csv")
+dat <- read.csv("./paper/plots/app1_N21_plot.csv",
                 header = FALSE)
+
 library(readr)
 library(ggplot2)
 library(dplyr)
 library(tibble)
 
-
+my_val <- unlist(dat)[-1] # the first one is the OAD
 theme_set(theme_bw())
 theme_update(plot.title = element_text(hjust = 0.5, size = 14),
              legend.text = element_text(size = 12),
              axis.text = element_text(size = 14),
              legend.title = element_blank(),
              title = element_text(size = 12))
-
-my_val <- unlist(dat)[-1] # the first one is the OAD
 tibble(val = my_val) %>% rowid_to_column("iter") %>% 
-  ggplot(aes(x = iter, y = val) ) + ylim(0, 0.25) + 
-  scale_y_continuous(trans='log10') +
-  geom_hline(yintercept = my_val[1], linetype="dashed", 
-             color = "red", size=0.5) + 
+  filter (!is.na(val)) %>% 
+  ggplot(aes(x = iter, y = val)) +  
   geom_line(col = "blue") + 
-  labs(x = "iteration", 
-       y = expression(paste("Loss function  ", Phi(xi[n]^(t)), "   (in log10 scale)"))) 
-  
+  scale_y_continuous(trans='log10') + 
+  geom_hline(yintercept=my_val[1], linetype="dashed", 
+             color = "red", size=0.5)
+  labs(x = "iteration", y = expression(paste("Loss function  ", Phi(xi[n]^(t)))))
 
-                     
